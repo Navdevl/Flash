f1c11ec704d962f5f5e4a35f0030115ef93f33ba
Naveen Honest Raj <naveen@skcript.com>
2018-04-20 23:14:07 +0530

    Beta release
    Signed-off-by: Naveen Honest Raj <naveen@skcript.com>

---
+++ reddit.py
@@ -0,0 +1,56 @@
+import praw
+import configparser
+import time
+from tinydb import TinyDB, Query
+
+
+CONFIG_FILE = "config.ini"
+
+class Reddit:
+  def __init__(self):
+    self.db = TinyDB('db.json')
+    self.db_query = Query()
+    self.get_credentials()
+    self.check_credentials()
+
+  def get_credentials(self):
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    self.client_id = config['default']['client_id']
+    self.client_secret = config['default']['client_secret']
+    self.user_agent = config['default']['user_agent']
+    self.username = config['default']['username']
+    self.password = config['default']['password']
+    self.favorite_subreddits = config['default']['favorite_subreddits'].split(',')
+    self.telegram_token = config['default']['telegram_token']
+
+  def check_credentials(self):
+    self.reddit = praw.Reddit(client_id=self.client_id,
+                              client_secret=self.client_secret,
+                              user_agent=self.user_agent,
+                              username=self.username,
+                              password=self.password
+                            )
+
+    try:
+      print("Welcome, {0} ".format(self.reddit.user.me()))
+    except:
+      print("Please check your credentials.")
+      exit(0)
+
+  def insert_to_db(self, subreddit, submission):
+    subreddit = self.reddit.subreddit(subreddit)
+    self.db.insert({'subreddit': subreddit.display_name, 'id': submission.id})
+
+  def get_latest_post(self):
+    print("Getting latest posts")
+    posts = []
+    for subreddit in self.favorite_subreddits:
+      for index, submission in enumerate(self.reddit.subreddit(subreddit).hot(limit=5)):
+        submission_results = self.db.search((self.db_query.id == submission.id) & (self.db_query.subreddit == subreddit))
+        if len(submission_results) == 0:
+          self.insert_to_db(subreddit, submission)
+          posts.append([subreddit, submission])
+      time.sleep(1)
+
+    return posts
---
+++ telebot.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Navdevl
+
+"""Telegram Bot that could fetch me what I wanted.."""
+
+from telegram.ext import Updater, CommandHandler
+import logging
+from reddit import Reddit
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+reddit = Reddit()
+
+def start(bot, update):
+    update.message.reply_text('Hey Dumbass! Use /set <seconds> to set an interval')
+
+def push(bot, job):
+    posts = reddit.get_latest_post()
+    for subreddit, post in posts:
+        message = "#{0}\n {1} \n {2}".format(subreddit, post.title, post.url)
+        bot.send_message(job.context, text=message)
+
+def set_timer(bot, update, args, job_queue, chat_data):
+    logger.info("Setting timer")
+    chat_id = update.message.chat_id
+    try:
+        interval = int(args[0])
+        if interval < 0:
+            update.message.reply_text('Sorry we can not go back to future!')
+            return
+        elif interval < 5:
+            update.message.reply_text('Interval should be minimum of 5 minutes. i.e. 300 seconds.')
+            return
+
+        job = job_queue.run_repeating(push, interval=interval, first=0, context=update.message.chat_id)
+
+        update.message.reply_text('Yes, my love ♥️ !')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /set <seconds>')
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """Run bot."""
+    updater = Updater(reddit.telegram_token)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", start))
+    dp.add_handler(CommandHandler("set", set_timer,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
